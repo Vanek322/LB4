1ac17224607a69a485cdc418d52e3b136ce0e11b
Филимонов Ваня <Fivan45@yandex.ru>
2025-04-03 14:03:22 +0500

    Установлены пакты, добавлено подключение к БД, изменены имена навигационных свойств

---
+++ LB4/Models/TitlesGenre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LB4.Models;
+
+public partial class TitlesGenre
+{
+    public int Id { get; set; }
+
+    public short IdGenre { get; set; }
+
+    public int IdAnime { get; set; }
+
+    //навигационные св-ва
+    public virtual AnimeTitle AnimeTitle { get; set; } = null!;
+
+    public virtual Genre Genre { get; set; } = null!;
+}
---
+++ LB4/Models/Type.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LB4.Models;
+
+public partial class Type
+{
+    public short Id { get; set; }
+
+    public string TypeName { get; set; } = null!;
+
+    //навигационные св-ва
+    public virtual ICollection<AnimeTitle> AnimeTitles { get; set; } = new List<AnimeTitle>();
+}
